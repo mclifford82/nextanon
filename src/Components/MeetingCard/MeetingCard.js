9dbaa25ac5129057c5e0beb47f15ff23e0964d3c
Martin Clifford <mclifford82@gmail.com>
2024-06-17 19:01:42 -0700

    Adding time until meeting

--- src/Components/MeetingCard/MeetingCard.js
+++ src/Components/MeetingCard/MeetingCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import './MeetingCard.scss';
 import { formatZoomMeetingCode } from '../../utils/utils';
 
@@ -9,11 +10,14 @@ function MeetingCard({ meeting: m }) {
   const local_minute = m.next_meeting_date_local_minute;
   const ampm = m.next_meeting_date_local_ampm;
 
+  let time_until = moment().to(m.next_meeting_date_local);
+  time_until = time_until.replace(/in |a /, '');
+
   return (
     <article className="information card">
       <h2 className="title">{m.name}</h2>
       <span className="tag">{m.meeting_type}</span>
-      <p className="smol">{local_weekday} @ {local_hour}:{local_minute} {ampm}</p>
+      <p className="smol">{local_weekday} @ {local_hour}:{local_minute} {ampm} <em className="smoler">({time_until})</em></p>
       <p className="info">{m.meeting_notes}</p>
       <p className="smol">{m.meeting_service}: { m.meeting_service === "Zoom" ? formatZoomMeetingCode(m.meeting_service_code) : m.meeting_service_code }</p>
       <p className="smol">Password: {m.meeting_pw}</p>
