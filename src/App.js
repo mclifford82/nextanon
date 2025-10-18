0ee23070751a4a825d0f0dfe603124a6b04cd667
Martin Clifford <mclifford82@gmail.com>
2025-10-17 23:00:06 -0700

    no kings bitches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './App.scss';
 
 function App() {
-  let daysClean;
-  // calculate days clean from 1/9/2023
-  const startDate = new Date('2024-01-09');
+  // calculate days clean from 1/9/2024 at 9:30 am
+  const startDate = new Date('2024-01-09T09:30:00');
   const currentDate = new Date();
-  const timeDiff = currentDate - startDate;
-  daysClean = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const diffTime = Math.abs(currentDate - startDate);
+  const daysClean = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  const dayWorkingOn = daysClean + 1;
 
   return (
     <>
-      <div class="noise"></div>
-      <div class="overlay"></div>
-      <div class="terminal">
-        <h1><span class="errorcode">{daysClean}</span> service unavailable</h1>
-        <p class="output">Nextanon has taken <span class="errorcode">42069</span> damage and has been felled.</p>
-        <p class="output">I'm no longer part of the TIAWO community and have no interest in promoting their meetings. That's all the detail I'm going into.</p>
-        <p class="output">Do whatever you need to do to stay off Kratom. Your recovery is your own. Guard it with your life.</p>
-        <p class="output">Deuces. ✌</p>
-      </div>
+      <img src="nokings.png" alt="Main" />
     </>
   );
 }
